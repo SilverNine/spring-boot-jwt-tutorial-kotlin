8eba95e140aa47d435485806f10cbfa2c536ce5e
SilverNine <akasilvernine@gmail.com>
2022-08-10 14:00:10 +0900

    feat: filterChain 주석 추가

--- src/main/kotlin/me/silvernine/tutorial/config/SecurityConfig.kt
+++ src/main/kotlin/me/silvernine/tutorial/config/SecurityConfig.kt
@@ -42,25 +42,34 @@ class SecurityConfig(
     @Bean
     @Throws(Exception::class)
     fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
-        httpSecurity // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+        httpSecurity
+            // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
             .csrf().disable()
+
             .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter::class.java)
+
             .exceptionHandling()
             .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-            .accessDeniedHandler(jwtAccessDeniedHandler) // enable h2-console
+            .accessDeniedHandler(jwtAccessDeniedHandler)
+
+            // enable h2-console
             .and()
             .headers()
             .frameOptions()
-            .sameOrigin() // 세션을 사용하지 않기 때문에 STATELESS로 설정
+            .sameOrigin()
+
+            // 세션을 사용하지 않기 때문에 STATELESS로 설정
             .and()
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
             .and()
             .authorizeRequests()
             .antMatchers("/api/hello").permitAll()
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/signup").permitAll()
             .anyRequest().authenticated()
+
             .and()
             .apply(JwtSecurityConfig(tokenProvider))
         return httpSecurity.build()
